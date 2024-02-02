de1da06765d5703c29a681027d198d7a179b54af
Nico Welles <nico@welles.io>
2024-02-02 22:37:11 +0100

    Trigger environment errors on startup

--- src/DoxieServer.Api/Program.cs
+++ src/DoxieServer.Api/Program.cs
@@ -3,7 +3,7 @@ using FastEndpoints;
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddFastEndpoints();
-builder.Services.AddSingleton<IEnvironmentService, EnvironmentService>();
+builder.Services.AddSingleton<IEnvironmentService>(new EnvironmentService());
 
 var app = builder.Build();
 app.UseFastEndpoints();
