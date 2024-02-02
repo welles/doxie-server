7ad643ecd5e9cff58d1efc049133e616510d53d5
Nico Welles <nico@welles.io>
2024-02-02 23:20:35 +0100

    Add LogInfo build target

--- src/DoxieServer.Build/Build.Clean.cs
+++ src/DoxieServer.Build/Build.Clean.cs
@@ -4,6 +4,7 @@ using Nuke.Common.Tools.DotNet;
 public partial class Build
 {
     Target Clean => d => d
+        .DependsOn(LogInfo)
         .Executes(() =>
         {
             DotNetTasks.DotNetClean(s => s.SetProject(CoreProject));
---
+++ src/DoxieServer.Build/Build.LogInfo.cs
@@ -0,0 +1,11 @@
+﻿using Nuke.Common;
+
+public partial class Build
+{
+    Target LogInfo => d=> d
+        .Executes(() =>
+        {
+            Serilog.Log.Information($"IsLocalBuild  = {IsLocalBuild}");
+            Serilog.Log.Information($"Configuration = {Configuration}");
+        });
+}
