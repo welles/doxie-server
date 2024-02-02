f543ef70fb31cc3d0bf3645009303ae4c216efb4
Nico Welles <nico@welles.io>
2024-02-02 23:08:06 +0100

    Add GitHub compile workflow

--- src/DoxieServer.Build/Build.cs
+++ src/DoxieServer.Build/Build.cs
@@ -1,12 +1,28 @@
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.ProjectModel;
 
+[GitHubActions(
+    nameof(Build.Compile),
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = new[]
+    {
+        "*"
+    },
+    FetchDepth = 0,
+    InvokedTargets = new []
+    {
+        nameof(Build.Compile)
+    }
+)]
 public partial class Build : NukeBuild
 {
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    GitHubActions GitHubActions => GitHubActions.Instance;
 
     [Solution(GenerateProjects = true)]
     readonly Solution Solution;
